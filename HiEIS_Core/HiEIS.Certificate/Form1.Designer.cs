af204d848fc8e07b436627dc738eedf4505d5b9b
dongtvse05344 <32121429+dongtvse05344@users.noreply.github.com>
2019-03-11 20:21:34 +0700

    Create WF for certificate

---
+++ HiEIS_Core/HiEIS.Certificate/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace HiEIS.Certificate
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSign = new System.Windows.Forms.Button();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSign
+            // 
+            this.btnSign.Location = new System.Drawing.Point(58, 208);
+            this.btnSign.Name = "btnSign";
+            this.btnSign.Size = new System.Drawing.Size(244, 31);
+            this.btnSign.TabIndex = 0;
+            this.btnSign.Text = "Kí";
+            this.btnSign.UseVisualStyleBackColor = true;
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(58, 137);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(244, 22);
+            this.txtCode.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Code :";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 410);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.btnSign);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSign;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
