6b8bd241443d734bb3b658dfd706b5393b52bc34
dongtvse05344 <32121429+dongtvse05344@users.noreply.github.com>
2019-03-11 20:52:52 +0700

    Update Template Certificate

--- HiEIS_Core/HiEIS.Certificate/Form1.Designer.cs
+++ HiEIS_Core/HiEIS.Certificate/Form1.Designer.cs
@@ -35,9 +35,10 @@
             // 
             // btnSign
             // 
-            this.btnSign.Location = new System.Drawing.Point(58, 208);
+            this.btnSign.Location = new System.Drawing.Point(87, 180);
+            this.btnSign.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSign.Name = "btnSign";
-            this.btnSign.Size = new System.Drawing.Size(244, 31);
+            this.btnSign.Size = new System.Drawing.Size(366, 40);
             this.btnSign.TabIndex = 0;
             this.btnSign.Text = "Kí";
             this.btnSign.UseVisualStyleBackColor = true;
@@ -45,30 +46,37 @@
             // 
             // txtCode
             // 
-            this.txtCode.Location = new System.Drawing.Point(58, 137);
+            this.txtCode.Location = new System.Drawing.Point(87, 114);
+            this.txtCode.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtCode.Name = "txtCode";
-            this.txtCode.Size = new System.Drawing.Size(244, 22);
+            this.txtCode.Size = new System.Drawing.Size(364, 30);
             this.txtCode.TabIndex = 1;
+            this.txtCode.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(58, 97);
+            this.label1.Location = new System.Drawing.Point(82, 74);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.Size = new System.Drawing.Size(71, 25);
             this.label1.TabIndex = 2;
             this.label1.Text = "Code :";
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(358, 410);
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(554, 300);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtCode);
             this.Controls.Add(this.btnSign);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Main";
-            this.Text = "Form1";
+            this.Text = "Certificate";
             this.ResumeLayout(false);
             this.PerformLayout();
 
